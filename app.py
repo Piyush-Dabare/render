c8b20ba3177bde816d996b8dedf91e87287cd53e
Piyush R. Dabare <89252943+Piyush-Dabare@users.noreply.github.com>
2025-02-21 17:00:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
-
-# Sample Data
-data = [
-    {"id": 1, "name": "John Doe", "age": 30, "city": "New York"},
-    {"id": 2, "name": "Jane Smith", "age": 25, "city": "Los Angeles"},
-    {"id": 3, "name": "Alice Johnson", "age": 28, "city": "Chicago"}
-]
 
 @app.route('/')
 def home():
@@ -18,7 +11,20 @@ def home():
 
 @app.route('/get-data', methods=['GET'])
 def get_data():
-    return jsonify(data)
+    n_records = 50
+    start_date = datetime(2023, 1, 1)
+    end_date = datetime(2024, 12, 31)
+    dates = pd.date_range(start=start_date, end=end_date).tolist()
+    name = ["a",'b','df','wr','wert','24']
+    # Sample Data
+    data = {
+    'Date': np.random.choice(dates, n_records),
+    'name':np.random.choice(name, n_records),
+    'phone': 0,
+    }
+    df = pd.DataFrame(data)
+    json_obj = df.to_json(orient="records")
+    return json_obj
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
